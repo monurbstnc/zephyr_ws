2f78cc373fdc61a2415542bbbb5cf9dc5208ba01
monurbstnc <meh.bstnc@gmail.com>
2024-11-14 09:06:33 +0300

    added.

--- drone_mainboard/icm42670/icm42670.c
+++ drone_mainboard/icm42670/icm42670.c
@@ -8,7 +8,12 @@ ICM42670_err_t icm42670_init(const struct i2c_dt_spec* dev_i2c)
 
     // Check WHO_AM_I register
     ret = i2c_reg_read_byte_dt(dev_i2c, WHO_AM_I, &reg_value);
-    if (ret != 0 || reg_value != 0x68)
+    /*
+    This register is used to verify the identity of the device. The contents of WHOAMI is an 8-bit device ID. The default
+    value of the register is 0x67. This is different from the I 2C address of the device as seen on the slave I2C controller
+    by the applications processor.
+    */
+    if (ret != 0 || reg_value != 0x67)
     {
         printk("Unexpected WHO_AM_I response: 0x%02x\n", reg_value);
         return ICM42670_ERR;
--- drone_mainboard/src/main.c
+++ drone_mainboard/src/main.c
@@ -16,30 +16,60 @@ static const int accel_hz = DT_PROP(ICM42670_NODE, accel_hz);
 static const int gyro_hz = DT_PROP(ICM42670_NODE, gyro_hz);
 static const int accel_fs = DT_PROP(ICM42670_NODE, accel_fs);
 static const int gyro_fs = DT_PROP(ICM42670_NODE, gyro_fs);
+static const int device_address = DT_PROP(ICM42670_NODE, reg);
 const struct i2c_dt_spec icm42670_dev = I2C_DT_SPEC_GET(ICM42670_NODE);
 
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED1_NODE DT_ALIAS(led2)
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+#define REDLED_NODE DT_ALIAS(led2)
+static const struct gpio_dt_spec red_led = GPIO_DT_SPEC_GET(REDLED_NODE, gpios);
+
+#define GREENLED_NODE DT_ALIAS(led2)
+static const struct gpio_dt_spec green_led = GPIO_DT_SPEC_GET(GREENLED_NODE, gpios);
+
+
 int main()
 {       
     int ret;
-    if (!device_is_ready(icm42670_dev.bus)) {
-        printk("ICM42670 device is not ready\n");
-        return -1;
-    }
-    
-
-	if (!gpio_is_ready_dt(&led)) {
+	if (!gpio_is_ready_dt(&red_led)) {
 		return 0;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+	ret = gpio_pin_configure_dt(&red_led, GPIO_OUTPUT_INACTIVE);
 	if (ret < 0) {
 		return 0;
 	}
 
-	gpio_pin_set_dt(&led,1);
+	gpio_pin_set_dt(&red_led,0);
+
+	//
+
+		if (!gpio_is_ready_dt(&green_led)) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&green_led, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	gpio_pin_set_dt(&green_led,0);
+
+    if (!device_is_ready(icm42670_dev.bus)) {
+        printk("ICM42670 device is not ready\n");
+		gpio_pin_set_dt(&red_led,1);
+        return -1;
+    }
+
+
+	if(icm42670_init(&icm42670_dev) != ICM42670_OK)
+	{
+		gpio_pin_set_dt(&red_led,1);
+		return -1;
+	}
+
+	gpio_pin_set_dt(&green_led,1);
+	k_msleep(2000);
+	gpio_pin_set_dt(&green_led,0);
+
     return 0;
 }
